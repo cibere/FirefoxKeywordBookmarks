e92c5ddb2c25c8fb32e357f6cd12b02c006a767e
cibere <71997063+cibere@users.noreply.github.com>
2024-10-30 20:34:53 -0700

    Update errors.py

--- Plugin/errors.py
+++ Plugin/errors.py
@@ -37,31 +37,38 @@ class PluginException(BasePluginException):
 class InternalException(BasePluginException):
     def __init__(self) -> None:
         opts = [
-            Option(score=100, icon="error", title="An internal error has occured."),
             Option(
-                score=80,
-                icon="github",
-                title="Please open a github issue",
-                sub="Click this to open github repository",
-                callback="open_url",
-                params=[
-                    "https://github.com/cibere/Flow.Launcher.Plugin.WordNikDictionary"
+                score=100,
+                icon="error",
+                title="An internal error has occured.",
+                sub="Open the context menu for more information",
+                context_data=[
+                    Option(
+                        score=80,
+                        icon="github",
+                        title="Please open a github issue",
+                        sub="Click this to open github repository",
+                        callback="open_url",
+                        params=[
+                            "https://github.com/cibere/Flow.Launcher.Plugin.FirefoxKeywordBookmarks"
+                        ],
+                    ),
+                    Option(
+                        score=79,
+                        icon="discord",
+                        title="Or create a thread in our discord server",
+                        sub="Click on this to open discord invite",
+                        callback="open_url",
+                        params=["https://discord.gg/y4STfDvc8j"],
+                    ),
+                    Option(
+                        score=0,
+                        icon="log_file",
+                        title="And provide your log file (FirefoxKeywordBookmarks.log)",
+                        sub="Click on this to open plugin folder.",
+                        callback="open_log_file_folder",
+                    ),
                 ],
-            ),
-            Option(
-                score=79,
-                icon="discord",
-                title="Or create a thread in our discord server",
-                sub="Click on this to open discord invite",
-                callback="open_url",
-                params=["https://discord.gg/y4STfDvc8j"],
-            ),
-            Option(
-                score=0,
-                icon="log_file",
-                title="And provide your log file (wordnik.log)",
-                sub="Click on this to open plugin folder.",
-                callback="open_log_file_folder",
             ),
         ]
         super().__init__("An Interal Error has occured.", opts)
